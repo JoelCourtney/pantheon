a59396f16e91ea736527022f6680efa11cf12c84
JoelCourtney <joel.e.courtney@gmail.com>
2020-07-05 09:55:59 -0600

    making deserializers

--- src/main/kotlin/IOWrapper.kt
+++ src/main/kotlin/IOWrapper.kt
@@ -1,17 +1,19 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import model.SpellSchool
+import model.SchoolDeserializer
 import java.nio.file.FileSystems
 import java.nio.file.Files
-import java.nio.file.Path
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 class IOWrapper {
     companion object {
         val mapper = ObjectMapper(YAMLFactory())
 
         init {
-            mapper.registerModule(KotlinModule())
+            val module = KotlinModule()
+            module.addDeserializer(SpellSchool::class.java, SchoolDeserializer())
+            mapper.registerModule(module)
         }
 
         inline fun<reified T: Any> read(dir: String, file: String): T {
--- src/main/kotlin/model/Spell.kt
+++ src/main/kotlin/model/Spell.kt
@@ -1,9 +1,25 @@
 package model
 
-data class Spell(
-    val name: String,
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import java.lang.IllegalArgumentException
 
-    val level: Int,
-    val school: String = "Nope"
+class Spell(
+    val name: String,
+    val level: Int
 ) {
+    val school: SpellSchool = SpellSchool.NONE
+}
+
+enum class SpellSchool(val symbol: String) {
+    EVOCATION("evocation"),
+    NONE("none");
+}
+
+class SchoolDeserializer : StdDeserializer<SpellSchool>(SpellSchool::class.java) {
+    override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): SpellSchool {
+        val s = p?.readValueAs(String::class.java) ?: throw IllegalArgumentException("Parser is null")
+        return SpellSchool.valueOf(s.toUpperCase())
+    }
 }
