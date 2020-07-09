12820566e73f25b38826607fda3851458ea14285
JoelCourtney <joel.courtney@colorado.edu>
2020-07-08 18:42:38 -0600

    well that was dumb

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,12 @@
 import io.JacksonWrapper
 import model.races.Race
-import model.races.VariantRace
+//import model.races.VariantRace
 import model.spells.Spell
 
 fun main() {
     val human = JacksonWrapper.readFile<Race>(
         "content-packs/official/Player's Handbook/Races/",
         "Variant Human.yaml"
-    ) as VariantRace
-    println(human.extraDescription)
+    )
+    println(human.name)
 }
--- src/main/kotlin/model/races/Race.kt
+++ src/main/kotlin/model/races/Race.kt
@@ -2,19 +2,20 @@ package model.races
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import io.deserializers.RaceDeserializer
+import io.deserializers.FileNameDeserializer.RaceFileNameDeserializer
 import model.results.Result
 
-@JsonDeserialize(using = RaceDeserializer::class)
 open class Race (
     val name: String,
     val traits: Array<Result>,
     val description: String,
-    open val chooseable: Boolean = true
+    open val chooseable: Boolean = true,
+    @JsonProperty("variant of")
+    @JsonDeserialize(using = RaceFileNameDeserializer::class)
+    val variantOf: Race? = null
 ) {
-    protected constructor(r: Race): this(r.name, r.traits, r.description)
     override fun toString(): String {
-        return "Race(name='$name', traits=${traits.contentToString()}, description='$description', chooseable=$chooseable)"
+        return "Race(name='$name', traits=${traits.contentToString()}, description='$description', chooseable=$chooseable, variantOf=$variantOf)"
     }
 
 }
