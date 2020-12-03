61f52b14e34992df19e3aff214aeabfe15763818
Joel Courtney <joel.e.courtney@gmail.com>
2020-12-03 01:15:19 -0700

    better configure

--- configure.py
+++ configure.py
@@ -4,9 +4,16 @@ import os
 
 
 def main():
+    print('Setting up uikit')
+    os.chdir('uikit')
+    os.system('git pull')
+    os.system('yarn install')
+
+    os.chdir('..')
     here = os.path.abspath(os.getcwd())
-    print('Creating uikit symlink')
+    print('\nCreating uikit symlink')
     os.symlink(here + '/src/www/uikit-custom', here + '/uikit/custom', True)
+
 
 
 if __name__ == '__main__':
