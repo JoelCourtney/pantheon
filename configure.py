93dcd9db55a7c917bc70ef21f60fbbdf6a4e153f
Joel Courtney <joel.e.courtney@gmail.com>
2020-12-02 23:23:00 -0700

    configure uikit script

--- configure.py
+++ configure.py
@@ -1,7 +1,12 @@
+#! /usr/bin/env python
+
 import os
 
+
 def main():
-    pass
+    here = os.path.abspath(os.getcwd())
+    os.symlink(here + '/src/www/uikit-custom', here + '/uikit/custom', True)
+
 
 if __name__ == '__main__':
     main()
