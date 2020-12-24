6edf3e6b8d1a7a043305083a0b795200cdc860b2
Joel Courtney <joel.e.courtney@gmail.com>
2020-12-23 21:08:56 -0700

    clean load process

--- src/www/scripts/load.js
+++ src/www/scripts/load.js
@@ -1,15 +1,25 @@
+/*
+Overall process:
+1. Load the modules given by 'module' tags in index.html. Each module consists of a mod.html file and a list of scripts. Currently only 'display.js' is loaded, but more scripts will be used later. Each display script must register a displayCallback function, which takes a character object and puts the relevant data into mod.html.
+2. Wait until all mod.html's are loaded, and all displayCallbacks are registered.
+3. call processContent() which does:
+4. call liftZIndices(), which allows dropdowns to exceed the boundaries of their sheet boxes. Otherwise they get truncated.
+5. call updateCharacter(). This queries the server for the character object. The readyStateChange callback will call callDisplayCallbacks() when it returns successfully.
+6. Wait for events to be triggered by modules. The modules will send requests to the server on their own when the user interacts with the UI. The response will be the updatedCharacter, which will be displayed again as before.
+*/
+
 function load() {
     loadModules();
 };
 
 /*
-Stolen from w3schools
+Shamelessly stolen from w3schools
 https://www.w3schools.com/howto/howto_html_include.asp
 
-modified to query all includes at time of calling, so the requests are parallel.
+modified to query all modules at time of calling, so the requests are parallel.
 */
-var includesRequested = 0;
-var includesReceived = 0;
+var modulesRequested = 0;
+var modulesReceived = 0;
 function loadModules() {
     /*loop through a collection of all HTML elements:*/
     let z = document.getElementsByTagName("div");
@@ -17,6 +27,7 @@ function loadModules() {
         let elmnt = z[i];
         /*search for elements with a certain atrribute:*/
         if (elmnt.hasAttribute("module")) {
+            modulesRequested++;
             let mod = "modules/" + elmnt.getAttribute("module") + "/";
             let file = mod + "mod.html";
             elmnt.removeAttribute("module");
@@ -26,38 +37,46 @@ function loadModules() {
                 if (this.readyState == 4) {
                     if (this.status == 200) {
                         elmnt.innerHTML = this.responseText;
-                        var displayScript = document.createElement('script');
-//                        displayScript.onload = function () {
-                            //do stuff with the script
-//                        };
-                        displayScript.src = mod + "display.js";
-                        document.head.appendChild(displayScript);
+
                     } else if (this.status == 404) {
                         elmnt.innerHTML = "Page not found.";
                     }
                     /*remove the attribute, and call this function once more:*/
-                    includesReceived++;
-                    if (includesReceived == includesRequested) {
-                        includesRequested = 0;
-                        includesReceived = 0;
-                        loadModules();
+                    modulesReceived++;
+                    if (areModulesLoaded()) {
+                        processContent();
                     }
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            includesRequested++;
+            let displayScript = document.createElement('script');
+            displayScript.src = mod + "display.js";
+            document.head.appendChild(displayScript);
         }
-    }
-    if (includesRequested == 0) {
-        contentLoaded()
     }
 };
 
-function contentLoaded() {
+function areModulesLoaded() {
+    return modulesRequested == modulesReceived && modulesRequested == displayCallbacks.length;
+}
+
+var displayCallbacks = [];
+function registerDisplayCallback(func) {
+    displayCallbacks.push(func);
+    if (areModulesLoaded()) {
+        processContent();
+    }
+}
+
+function callDisplayCallbacks(character) {
+    displayCallbacks.forEach(func => func(character));
+}
+
+function processContent() {
     liftZIndices();
     // somehow wait until all scripts are loaded.
-    getCharacter();
+    updateCharacter();
 };
 
 function liftZIndices() {
@@ -73,15 +92,7 @@ function liftZIndices() {
     }
 }
 
-function getCharacter() {
+function updateCharacter() {
     // TODO("this, later")
 }
 
-var displayCallbacks = [];
-function registerDisplayCallback(func) {
-    displayCallbacks.push(func);
-}
-
-function distributeToDisplayCallbacks(character) {
-    displayCallbacks.forEach(func => func(character));
-}
