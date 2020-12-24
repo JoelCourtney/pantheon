b85a1efc29af27a802c2018807b6ca3ece9e4d39
Joel Courtney <joel.e.courtney@gmail.com>
2020-12-23 21:12:29 -0700

    lifting z-indices no longer needed

--- src/www/scripts/load.js
+++ src/www/scripts/load.js
@@ -2,10 +2,8 @@
 Overall process:
 1. Load the modules given by 'module' tags in index.html. Each module consists of a mod.html file and a list of scripts. Currently only 'display.js' is loaded, but more scripts will be used later. Each display script must register a displayCallback function, which takes a character object and puts the relevant data into mod.html.
 2. Wait until all mod.html's are loaded, and all displayCallbacks are registered.
-3. call processContent() which does:
-4. call liftZIndices(), which allows dropdowns to exceed the boundaries of their sheet boxes. Otherwise they get truncated.
-5. call updateCharacter(). This queries the server for the character object. The readyStateChange callback will call callDisplayCallbacks() when it returns successfully.
-6. Wait for events to be triggered by modules. The modules will send requests to the server on their own when the user interacts with the UI. The response will be the updatedCharacter, which will be displayed again as before.
+3. call updateCharacter(). This queries the server for the character object. The readyStateChange callback will call callDisplayCallbacks() when it returns successfully.
+4. Wait for events to be triggered by modules. The modules will send requests to the server on their own when the user interacts with the UI. The response will be the updated Character, which will be displayed again as before.
 */
 
 function load() {
@@ -44,7 +42,7 @@ function loadModules() {
                     /*remove the attribute, and call this function once more:*/
                     modulesReceived++;
                     if (areModulesLoaded()) {
-                        processContent();
+                        updateCharacter();
                     }
                 }
             }
@@ -65,31 +63,12 @@ var displayCallbacks = [];
 function registerDisplayCallback(func) {
     displayCallbacks.push(func);
     if (areModulesLoaded()) {
-        processContent();
+        updateCharacter();
     }
 }
 
 function callDisplayCallbacks(character) {
     displayCallbacks.forEach(func => func(character));
-}
-
-function processContent() {
-    liftZIndices();
-    // somehow wait until all scripts are loaded.
-    updateCharacter();
-};
-
-function liftZIndices() {
-    var offset = 0;
-    let drops = document.getElementsByClassName("uk-dropdown");
-    for (var e of drops) {
-        let z = window.getComputedStyle(e).zIndex;
-        while (e.parentElement.id != "sheet") {
-            e = e.parentElement;
-        }
-        e.style.zIndex = z - offset;
-        offset++;
-    }
 }
 
 function updateCharacter() {
