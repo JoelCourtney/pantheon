e821afd3c6e5f394dd5daa9b0af3ebf7cd864fef
Joel Courtney <joel.e.courtney@gmail.com>
2021-01-04 16:26:32 -0700

    starting rust

---
+++ server/src/character.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub(crate) struct StoredCharacter {
+    name: String,
+
+    health: u64,
+    temp_health: u64,
+
+    base_strength: u8,
+    base_dexterity: u8,
+    base_constitution: u8,
+    base_intelligence: u8,
+    base_wisdom: u8,
+    base_charisma: u8,
+}
+
+impl StoredCharacter {
+    pub fn copy_to_resolved(self) -> ResolvedCharacter {
+        ResolvedCharacter {
+            name: self.name,
+            health: self.health,
+            temp_health: self.temp_health,
+
+            strength: self.base_strength,
+            dexterity: self.base_dexterity,
+            constitution: self.base_constitution,
+            intelligence: self.base_intelligence,
+            wisdom: self.base_wisdom,
+            charisma: self.base_charisma,
+
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub(crate) struct ResolvedCharacter {
+    name: String,
+
+    health: u64,
+    temp_health: u64,
+    max_health: u64,
+
+    strength: u8,
+    dexterity: u8,
+    constitution: u8,
+    intelligence: u8,
+    wisdom: u8,
+    charisma: u8,
+
+    strength_modifier: i8,
+    dexterity_modifier: i8,
+    constitution_modifier: i8,
+    intelligence_modifier: i8,
+    wisdom_modifier: i8,
+    charisma_modifier: i8,
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
+mod character;
+
+use character::{StoredCharacter, ResolvedCharacter};
+
 fn main() {
-    println!("Hello, world!");
+    let json = std::fs::read_to_string("test.json").unwrap();
+    println!("{}", json);
+    let char: StoredCharacter = serde_json::from_str(&json).unwrap();
+    println!("{:?}", char);
+
+    let res = char.copy_to_resolved();
+    println!("{:?}", res);
 }
