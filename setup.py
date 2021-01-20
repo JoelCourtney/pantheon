407f5bde093b6b7f64788dacb0ed51ee78d6638c
Joel Courtney <joel.e.courtney@gmail.com>
2021-01-20 00:04:02 -0700

    setup generates secret key

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #! /usr/bin/env python3
 
 import os
+import binascii
 
 
 def main():
+    print('Setting up secret key')
+    key = str(binascii.b2a_base64(os.urandom(32)))
+    template = open('src/Rocket.toml.template', 'r').read()
+    output = template.replace('%%%SECRET%%%', key[2:-3])
+    open('Rocket.toml', 'w').write(output)
+
     print('Setting up uikit')
     os.chdir('src/www/uikit')
     os.system('git submodule init')
@@ -21,7 +28,6 @@ def main():
         print('\nCreating uikit symlink:')
     print(src + " -> " + dst)
     os.symlink(src, dst, True)
-
 
 
 if __name__ == '__main__':
