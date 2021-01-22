e47ab47aa5d29d68db0f15e7fcf12e2a8012d4bf
Joel Courtney <joel.e.courtney@gmail.com>
2021-01-22 11:58:00 -0700

    fix money

--- src/www/modules/money/display.js
+++ src/www/modules/money/display.js
@@ -1,7 +1,7 @@
 registerDisplayCallback(function (character) {
-    setField("platinum", character.money[0]);
-    setField("gold", character.money[1]);
+    setField("copper", character.money[0]);
+    setField("silver", character.money[1]);
     setField("electrum", character.money[2]);
-    setField("silver", character.money[3]);
-    setField("copper", character.money[4]);
+    setField("gold", character.money[3]);
+    setField("platinum", character.money[4]);
 });
