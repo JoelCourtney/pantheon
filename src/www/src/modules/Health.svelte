1626023a13c6ec8f9a65334f7f524559f52b4627
Joel Courtney <joel.e.courtney@gmail.com>
2021-02-05 12:37:20 -0700

    health box height fix

--- src/www/src/modules/Health.svelte
+++ src/www/src/modules/Health.svelte
@@ -4,18 +4,16 @@
 </script>
 
 <div class="sheet-box uk-width-auto">
-    <div uk-grid class="uk-grid-medium uk-child-width-1-3">
-        <div>
+        <div class="uk-display-inline-block uk-margin-right">
             <h1 class="sheet-box-title-small">Health</h1>
             <p class="sheet-box-number">{c.health}</p>
         </div>
-        <div>
+        <div class="uk-display-inline-block uk-margin-right">
             <h1 class="sheet-box-title-small">Max</h1>
             <p class="sheet-box-number">{c.max_health}</p>
         </div>
-        <div>
+        <div class="uk-display-inline-block">
             <h1 class="sheet-box-title-small">Temp</h1>
             <p class="sheet-box-number">{c.temp_health}</p>
         </div>
-    </div>
 </div>
