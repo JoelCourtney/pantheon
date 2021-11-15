eed5bbdcc5b7bd03b682483b78904a8cbf64084c
Joel Courtney <joel.e.courtney@gmail.com>
2021-11-15 09:43:03 -0700

    start actix

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
-    let s = include_bytes!("../../client/dist/index-d3e01dc56314a796_bg.wasm");
-    println!("Hello, wold!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
