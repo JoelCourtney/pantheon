b65f1dc11299d0b85761c4082f439c6fdac726ce
Joel Courtney <joel.e.courtney@gmail.com>
2021-11-26 12:38:29 -0700

    improved query; start geometry

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,6 +4,8 @@ mod macros;
 use seed::{*, prelude::*};
 use character::StoredCharacter;
 use std::path::PathBuf;
+use reqwest::Body;
+use serde::de::DeserializeOwned;
 
 struct Model {
     base: BaseLayer,
@@ -38,15 +40,9 @@ enum Msg {
 
 fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
     log!(url.to_string());
-    character::try_it();
+    character::try_it().unwrap();
     orders.perform_cmd(async {
-        let client = reqwest::Client::new();
-        let res = client.post("http://localhost:8080/list")
-            .send()
-            .await.unwrap();
-        let bytes = res.bytes().await.unwrap();
-        log!("bytes: {:?}", bytes);
-        let list = bincode::deserialize(&bytes).unwrap();
+        let list: Vec<PathBuf> = query("list", "").await.unwrap();
         log!(&list);
         Msg::SetBase(BaseLayer::List(RequestProgress::Succeeded(list)))
     });
@@ -70,4 +66,13 @@ fn view(model: &Model) -> impl IntoNodes<Msg> {
 
 fn main() {
     App::start("app", init, update, view);
+}
+
+async fn query<T: DeserializeOwned>(path: impl AsRef<str>, body: impl Into<Body>) -> Result<T,()> {
+    let client = reqwest::Client::new();
+    let res = client.post(format!("{}/{}", seed::window().location().origin().unwrap(), path.as_ref()))
+        .body(body).send()
+        .await.map_err(|_| ())?;
+    let bytes = res.bytes().await.map_err(|_| ())?;
+    Ok(bincode::deserialize(&bytes).map_err(|_| ())?)
 }
