d8de7aadb08cbf96f9327ff1ff80e92d6c06f01a
joelco <joel.courtney@jpl.nasa.gov>
2022-03-22 15:07:14 -0600

    Delete server_macros

--- server_macros/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod server;
-
-#[proc_macro]
-pub fn match_raw_files(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast: syn::ExprArray = syn::parse(input).expect("expected expr array");
-    server::match_raw_files(ast)
-}
