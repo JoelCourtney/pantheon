f210d6cacfc62fb07caa0f6278bb21aadb30d95d
JoelCourtney <joel.e.courtney@gmail.com>
2022-11-27 10:41:33 -0700

    Add permissive cors

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 pub mod routes;
 
 use actix_web::{HttpServer};
-use actix_web::dev::Server;
+use actix_cors::Cors;
 
 pub fn new(port: u16) -> std::io::Result<Server> {
     Ok(HttpServer::new(|| {
+        let cors = Cors::permissive();
+
         actix_web::App::new()
-            .service(routes::health)
+            .wrap(cors)
+            .service(routes::list)
     })
         .bind(("127.0.0.1", port))?
         .run())
